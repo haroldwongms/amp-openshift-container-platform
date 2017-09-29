2d35325315043f8d7ed78107d69e9be838e666e7
haroldwongms <harold.wong@microsoft.com>
2017-09-29 15:34:08 -0700

    Initial Commit
    Includes working files for OCP 3.5

---
+++ README.md
@@ -0,0 +1,2 @@
+# OpenShift Container Platform for Azure Marketplace
+OpenShift Container Platform Solution Templates for Azure Marketplace
---
+++ scripts/bastionPrep.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+echo $(date) " - Starting Bastion Prep Script"
+
+RHSMUSERNAME=$1
+RHSMPASSWORD="$2"
+POOL_ID=$3
+
+# Remove RHUI
+
+rm -f /etc/yum.repos.d/rh-cloud.repo
+sleep 10
+
+# Register Host with Cloud Access Subscription
+echo $(date) " - Register host with Cloud Access Subscription"
+
+subscription-manager register --username="$RHSMUSERNAME" --password="$RHSMPASSWORD"
+
+if [ $? -eq 0 ]
+then
+   echo "Subscribed successfully"
+else
+   echo "Incorrect Username or Password specified"
+   exit 3
+fi
+
+subscription-manager attach --pool=$POOL_ID > attach.log
+if [ $? -eq 0 ]
+then
+   echo "Pool attached successfully"
+else
+   evaluate=$( cut -f 2-5 -d ' ' attach.log )
+   if [[ $evaluate == "unit has already had" ]]
+      then
+         echo "Pool $POOL_ID was already attached and was not attached again."
+	  else
+         echo "Incorrect Pool ID or no entitlements available"
+         exit 4
+   fi
+fi
+
+# Disable all repositories and enable only the required ones
+echo $(date) " - Disabling all repositories and enabling only the required repos"
+
+subscription-manager repos --disable="*"
+
+subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.5-rpms" \
+    --enable="rhel-7-fast-datapath-rpms"
+
+# Install base packages and update system to latest packages
+echo $(date) " - Install base packages and update system to latest packages"
+
+yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion httpd-tools nodejs
+yum -y update --exclude=WALinuxAgent
+
+# Ensure proper repos are still enabled
+subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.5-rpms" \
+    --enable="rhel-7-fast-datapath-rpms" 
+
+yum -y install atomic-openshift-excluder atomic-openshift-docker-excluder
+
+atomic-openshift-excluder unexclude
+
+# Install OpenShift utilities
+echo $(date) " - Installing OpenShift utilities"
+
+yum -y install atomic-openshift-utils
+
+# Create playbook to update ansible.cfg file to include path to library
+
+cat > updateansiblecfg.yaml <<EOF
+#!/usr/bin/ansible-playbook
+
+- hosts: localhost
+  gather_facts: no
+  tasks:
+  - lineinfile:
+      dest: /etc/ansible/ansible.cfg
+      regexp: '^library '
+      insertafter: '#library        = /usr/share/my_modules/'
+      line: 'library = /usr/share/ansible/openshift-ansible/library/'
+EOF
+
+# Run Ansible Playbook to update ansible.cfg file
+
+echo $(date) " - Updating ansible.cfg file"
+
+ansible-playbook ./updateansiblecfg.yaml
+
+# Install Azure CLI
+
+echo $(date) " - Installing Azure CLI"
+
+npm install -g azure-cli
+
+echo $(date) " - Script Complete"
