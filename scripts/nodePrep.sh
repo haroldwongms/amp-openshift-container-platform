441f7ba692f5944e4a75709c45f1f2dde49cb9b7
haroldwongms <harold.wong@microsoft.com>
2017-09-30 00:23:21 -0700

    OSE 3.6 repos
    Forgot to update second instance of enabling OSE 3.6 repo

--- scripts/nodePrep.sh
+++ scripts/nodePrep.sh
@@ -59,7 +59,7 @@ yum -y update --exclude=WALinuxAgent
 subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
-    --enable="rhel-7-server-ose-3.5-rpms" \
+    --enable="rhel-7-server-ose-3.6-rpms" \
     --enable="rhel-7-fast-datapath-rpms" 
 
 yum -y install atomic-openshift-excluder atomic-openshift-docker-excluder
